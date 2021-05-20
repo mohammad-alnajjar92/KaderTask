bf7776e32eb3373a2ce19162f2814fa3f94b7e3d
mohammad-alnajjar92 <70472723+mohammad-alnajjar92@users.noreply.github.com>
2021-05-20 16:41:45 +0300

    optimize store space

--- app/models/moviesStore/moviesStore.ts
+++ app/models/moviesStore/moviesStore.ts
@@ -7,38 +7,38 @@ const arrayOfStatus = ["idle", "pending", "done", "error"];
 /**
  * login step
  */
- const movieModel = types.model('movieModel').props({
-    adult: types.optional(types.boolean,false),
-    backdrop_path: types.optional(types.string,''),
+const movieModel = types.model('movieModel').props({
+    adult: types.optional(types.boolean, false),
+    backdrop_path: types.optional(types.string, ''),
     genre_ids: types.array(types.number),
-    id: types.optional(types.number,0),
-    original_language: types.optional(types.string,''),
-    original_title: types.optional(types.string,''),
-    overview:types.optional(types.string,''),
-    popularity: types.optional(types.number,0),
-    poster_path: types.optional(types.string,''),
-    release_date: types.optional(types.string,''),
-    title: types.optional(types.string,''),
-    video: types.optional(types.boolean,false),
-    vote_average:types.optional(types.number,0),
-    vote_count: types.optional(types.number,0),
-  });
+    id: types.optional(types.number, 0),
+    original_language: types.optional(types.string, ''),
+    original_title: types.optional(types.string, ''),
+    overview: types.optional(types.string, ''),
+    popularity: types.optional(types.number, 0),
+    poster_path: types.optional(types.string, ''),
+    release_date: types.optional(types.string, ''),
+    title: types.optional(types.string, ''),
+    video: types.optional(types.boolean, false),
+    vote_average: types.optional(types.number, 0),
+    vote_count: types.optional(types.number, 0),
+});
 
 const genreModel = types.model('genreModel').props({
     id: types.number,
-    name: types.optional(types.string,'')
-  });
+    name: types.optional(types.string, '')
+});
 
- 
+
 export const MoviesStoreModel = types
     .model("MoviesStore")
     .props({
         status: types.optional(types.enumeration(arrayOfStatus), "idle"),
-        movies: types.optional(types.array(movieModel),[]),
-        genres: types.optional(types.array(genreModel),[]),
-        cast:types.optional(types.array(types.frozen()),[]),
-        crew:types.optional(types.array(types.frozen()),[]),
-        movieDetail:types.optional(types.frozen(),{}),
+        movies: types.optional(types.array(movieModel), []),
+        genres: types.optional(types.array(genreModel), []),
+        cast: types.optional(types.array(types.frozen()), []),
+        crew: types.optional(types.array(types.frozen()), []),
+        movieDetail: types.optional(types.frozen(), {}),
 
     }).views(self => ({
         get isLoading() {
@@ -61,23 +61,22 @@ export const MoviesStoreModel = types
         setStatus(value?: "idle" | "pending" | "done" | "error") {
             self.status = value
         },
-        updateMovies(movies){
-            self.movies=movies;
+        updateMovies(movies) {
+            self.movies = movies;
         },
-        updateGenres(genres){
-            self.genres=genres;
+        updateGenres(genres) {
+            self.genres = genres;
         },
-        updateMovieDetails(movieDetail){
-            self.movieDetail=movieDetail;
+        updateMovieDetails(movieDetail) {
+            self.movieDetail = movieDetail;
         },
-        updateCast(cast){
-            self.cast=cast;
-        }, 
-        updateCrew(crew){
-            self.crew=crew;
+        updateCast(cast) {
+            self.cast = cast;
+        },
+        updateCrew(crew) {
+            self.crew = crew;
         },
     })).actions(self => ({
-
         getGenres: flow(function* () {
             try {
                 const genre = yield self.environment.api.getGenres()
@@ -85,11 +84,8 @@ export const MoviesStoreModel = types
             }
             catch (error) {
                 console.log(error)
-
             }
-
         }),
-
         syncMovies: flow(function* () {
             try {
                 const res = yield self.environment.api.upcomingMovies()
@@ -97,62 +93,41 @@ export const MoviesStoreModel = types
             }
             catch (error) {
                 console.log(error)
-
             }
-
         }),
-
         popularMovies: flow(function* () {
             console.log("popularMovies")
             try {
                 const res = yield self.environment.api.popularMovies()
                 self.updateMovies(res.movies)
-
             }
             catch (error) {
                 console.log(error)
-
             }
-
         }),
-
         topMovies: flow(function* () {
             try {
                 const res = yield self.environment.api.topMovies()
                 console.log('*****res', res)
                 self.updateMovies(res.movies)
-
             }
             catch (error) {
                 console.log(error)
-
             }
-
         }),
-
-        movieDetails:flow(function* (id){
-            try{
+        movieDetails: flow(function* (id) {
+            try {
                 const res = yield self.environment.api.movieDetails(id)
                 const cridet = yield self.environment.api.movieCridets(id)
-                console.log("*****cridet",cridet)
                 self.updateMovieDetails(res.movieDetails)
                 self.updateCast(cridet.cast)
                 self.updateCrew(cridet.crew)
-
-        
             }
-            catch(error){
+            catch (error) {
                 console.log(error)
             }
         }),
-
-
     }))
-    .actions(self => ({
-
-    }))
-
-
 
 
 
